0662ad4518f611e23b81fc83ae5dd552b25fac41
Vinicius Pacheco <vfpweb@gmail.com>
2014-10-09 18:39:40 -0300

    first commit

---
+++ basico/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+//comentário simple. Para comentário em bloco /**/
+func main() {
+	fmt.Println("Hello World")
+}
---
+++ basico/hello2.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//Por padrão go é utf-8
+func main() {
+	fmt.Printf("Hora atual é: %s", time.Now())
+}
